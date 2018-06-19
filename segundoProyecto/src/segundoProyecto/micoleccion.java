951fefc9a643b30c2ac411af1d54bbae9ac2c515
alexp <alexp@DESKTOP-9QI0DET>
2018-06-19 11:31:17 +0200

    1 version

---
+++ segundoProyecto/src/segundoProyecto/micoleccion.java
@@ -0,0 +1,26 @@
+package segundoProyecto;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class micoleccion {
+
+	public static void main(String[] args) {
+		
+		Set coleccionSet=new HashSet();
+		coleccionSet.add("Matrix");
+		coleccionSet.add("Hola");
+		coleccionSet.add("Adios");
+		coleccionSet.add("Pos nah");
+		coleccionSet.add("Para tu casa tonto");
+		
+		for(Object elemento : coleccionSet) {
+			
+			
+			
+		}
+		
+
+	}
+
+}
